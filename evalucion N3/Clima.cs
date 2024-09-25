5005f6facb36c9f683cda3fc723e251d15bc3673
Lucasmurua19 <veleznahuel515@gmail.com>
2024-09-24 21:21:52 -0300

    Agregar archivos de proyecto.

---
+++ evalucion N3/Clima.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PronosticoClimatico
+{
+    public class Clima
+    {
+        private static Random random = new Random();
+
+
+        public string[] GenerarPronostico(int dias)
+        {
+            string[] pronosticos = new string[dias];
+
+
+            for (int i = 0; i < dias; i++)
+            {
+                int climaAleatorio = random.Next(1, 4);
+
+
+                switch (climaAleatorio)
+                {
+                    case 1:
+                        pronosticos[i] = $"Día {i + 1}: Soleado";
+                        break;
+                    case 2:
+                        pronosticos[i] = $"Día {i + 1}: Lluvioso";
+                        break;
+                    case 3:
+                        pronosticos[i] = $"Día {i + 1}: Nublado";
+                        break;
+                    default:
+                        pronosticos[i] = $"Día {i + 1}: Clima desconocido";
+                        break;
+                }
+            }
+
+            return pronosticos;
+        }
+    }
+}
---
+++ evalucion N3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using PronosticoClimatico;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evalucion_N3
+{
+    public partial class Form1 : Form
+    {
+        private Clima clima;
+        public Form1()
+        {
+            InitializeComponent();
+            clima = new Clima(); 
+
+        }
+     
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+     
+            if (int.TryParse(textBox1.Text, out int dias) && dias > 0)
+            {
+
+                lstClima.Items.Clear();
+
+               
+                string[] pronosticos = clima.GenerarPronostico(dias);
+
+                foreach (var pronostico in pronosticos)
+                {
+                    lstClima.Items.Add(pronostico);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido de días.");
+            }
+        }
+
+ 
+    }
+}
+
